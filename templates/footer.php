393dcce9d06181c847e4cc8f0309d4346f6cacc5
koerner <mkoerner@uni-koblenz.de>
2013-03-29 23:38:51 +0100

    modified footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,11 @@
   <div class="footer">
     <div class="container">
       <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+      &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+      <div class="theme-info">
+        Powered by <a href="http://wordpress.org/">WordPress</a>.
+        Theme based on <a href="http://www.rootstheme.com/">Roots</a>.
+      </div>
     </div>
   </div>
 </footer>
