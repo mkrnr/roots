711c2895108cf54965da8c997b633e534f3597d3
koerner <mkoerner@uni-koblenz.de>
2013-03-31 16:40:18 +0200

    added new page for RebelMouse feed

--- lib/config.php
+++ lib/config.php
@@ -64,7 +64,7 @@ function roots_display_sidebar() {
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'page-custom.php'
+      'page-custom.php', 'page-rebelmouse.php'
     )
   );
 
---
+++ page-rebelmouse.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: RebelMouse
+*/
+?>
+
+<?php get_template_part('templates/content', 'page'); ?>
