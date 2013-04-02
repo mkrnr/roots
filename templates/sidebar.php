1836a6ae8094325fd751ff2b0bf2f873eda60c71
koerner <mkoerner@uni-koblenz.de>
2013-04-02 21:23:02 +0200

    added Content field to sidebar.php

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,3 +1,9 @@
-<div class="sidebar-inner">
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<?php if (is_category()) { ?>
+  <div class="sidebar-inner box-colored sidebar-info">
+    <h3>Content</h3>
+    <?php echo category_description( $category_id ); ?>
+  </div>
+<? } ?>
+<div class="sidebar-inner box-colored">
+  <?php dynamic_sidebar('sidebar-primary'); ?>
 </div>
