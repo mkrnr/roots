f3c39b8044fda272f749f3d30aaee4a33d831458
mkrnr <info@mkoerner.de>
2013-12-26 12:24:19 +0100

    Update editor-style.css (default values)

--- lib/scripts.php
+++ lib/scripts.php
@@ -16,6 +16,7 @@
 function roots_scripts() {
   wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
   wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
+  wp_enqueue_style('roots_editor-style', get_template_directory_uri() . '/assets/css/editor-style.css', false, null);
 
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
